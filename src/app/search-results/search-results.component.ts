71da31e332ad10d39b21a4d407c4133c5d77ea75
dsteins <dsteinschreiber@gmail.com>
2022-06-10 18:16:35 -0500

    Sample search results

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -7,6 +7,13 @@ export interface Tile {
   text: string;
 }
 
+interface RestaurantData {
+  pictureUrl?: string;
+  name: string;
+  description: string;
+  rating: number;
+}
+
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
@@ -16,16 +23,27 @@ export class SearchResultsComponent implements OnInit {
 
   @Input() searchString = "";
 
-  constructor() { }
+  searchResult: Array<RestaurantData> = [];
+
+  sampleResults = [
+    {
+      name: "Little Italy",
+      description: "Restaurant description for some italian restaurant",
+      rating: 4.5
+    },
+    {
+      name: "Danny's Burgers",
+      description: "Best burgers in town!",
+      rating: 5.0
+    }
+  ]
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     console.log("Results loaded", this.searchString);
+    this.searchResult = this.sampleResults;
   }
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
 }
